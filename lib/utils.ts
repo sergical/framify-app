2ebf95d8cfcc2070061e6d84a5ef91bc140e95d6
Sergiy Dybskiy <s@serg.tech>
2024-03-23 23:55:33 -0400

    slugify

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,13 +1,14 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-
+import slugify from "slugify";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export function convertToSlug(text: string): string {
-  return text
-    .toLowerCase() // convert the whole string to lowercase
-    .trim() // remove whitespace from both ends of a string
-    .replace(/\s+/g, "-"); // replace spaces with hyphens
+  return slugify(text, {
+    lower: true,
+    strict: true,
+    remove: /[*+~.()'"!:@]/g,
+  });
 }
